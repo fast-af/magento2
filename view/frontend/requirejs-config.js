f4568fa10eba944db0e7442d39c2d91013ae05d9
Cory Webb <cory.webb@fast.co>
2021-12-21 13:47:48 -0600

    Require fastUiComponent

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,6 +2,7 @@ var config = {
     map: {
         '*': {
             'fastConfig': 'Fast_Checkout/js/fast-config',
+            'fastUiComponent': 'Fast_Checkout/js/fast-ui-component',
             'clearCart': 'Fast_Checkout/js/clear-cart',
             'fastCheckout': 'Fast_Checkout/js/fast-checkout',
             'fastCheckoutGrouped': 'Fast_Checkout/js/fast-checkout-grouped'
